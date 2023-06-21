3c8dc34a36e5a146401933bf4a112cd1fd158a4a
Pranay Kothari <pranay.kothari109@gmail.com>
2023-06-21 10:39:01 +0530

    read files from directory

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
+// error handler
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func countGoFiles(folder string) (count int) {
+	files, err := os.ReadDir(folder)
+	check(err)
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+		if path.Ext(f.Name()) == ".exe" {
+			count++
+		}
+	}
+	return count
+}
 
 func main() {
 	fmt.Println("welcome to file organizer!")
+
+	// read files in folder
+	directory := "."
+	files, err := os.ReadDir(directory)
+	check(err)
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
+
 }

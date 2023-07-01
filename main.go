39ec12a0558238d5f9aa1cb52eafc510162810e0
Pranay Kothari <pranay.kothari109@gmail.com>
2023-07-01 16:24:55 +0530

    file organization working successfully

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
 )
 
 // error handler
@@ -13,29 +14,50 @@ func check(e error) {
 	}
 }
 
-func countGoFiles(folder string) (count int) {
-	files, err := os.ReadDir(folder)
-	check(err)
-
-	for _, f := range files {
-		fmt.Println(f.Name())
-		if path.Ext(f.Name()) == ".exe" {
-			count++
-		}
+func getDirectory(extension string) string {
+	if extension == ".exe" {
+		return "executables"
+	} else if extension == ".jpg" || extension == ".jpeg" || extension == ".png" {
+		return "images"
+	} else if extension == ".mp3" || extension == ".wav" {
+		return "audio"
+	} else if extension == ".mp4" || extension == ".avi" || extension == ".mov" {
+		return "video"
+	} else if extension == ".doc" || extension == ".docx" {
+		return "documents"
+	} else if extension == ".pdf" {
+		return "pdf"
+	} else if extension == ".zip" || extension == ".rar" {
+		return "compressed"
+	} else if extension == ".xlsx" || extension == ".csv" {
+		return "documents"
+	} else if extension == ".html" || extension == ".htm" {
+		return "html"
+	} else if extension == ".css" {
+		return "css"
+	} else if extension == ".js" {
+		return "javascript"
+	} else {
+		return "others"
 	}
-	return count
 }
 
 func main() {
 	fmt.Println("welcome to file organizer!")
 
+	var sourceDirectory = "e:/file-organizer-test" // source directory
 	// read files in folder
-	directory := "."
-	files, err := os.ReadDir(directory)
+	files, err := os.ReadDir(sourceDirectory)
 	check(err)
 
 	for _, f := range files {
-		fmt.Println(f.Name())
+		var source = filepath.Join(sourceDirectory, f.Name())
+		var destDir = filepath.Join(sourceDirectory, getDirectory(path.Ext(f.Name())))
+		var destination = filepath.Join(destDir, f.Name())
+		err = os.MkdirAll(destDir, os.ModePerm)
+		err := os.Rename(source, destination)
+		check(err)
 	}
 
+	fmt.Println("Succefully organized the files!")
 }
